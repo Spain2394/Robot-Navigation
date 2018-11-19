a90e8eebc67f43dfca5bf43f79539dff22844dc2
Parallels <aspain2394@gmailom>
2018-11-19 13:08:28 -0500

    pkg

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/parallels/Robot-Navigation/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/parallels/Robot-Navigation/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parallels/Robot-Navigation/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/parallels/Robot-Navigation/src:$ROS_PACKAGE_PATH"
